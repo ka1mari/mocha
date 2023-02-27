98adb6c51a7cbc19c8e5735e021529080da5c7ae
mari <tsujianii@tuta.io>
2023-02-27 20:50:52 +0800

    simply dont panic within panic

--- crates/std/src/panic.rs
+++ crates/std/src/panic.rs
@@ -2,28 +2,13 @@ use crate::{
     io::{self, Write},
     process,
 };
-use core::{
-    panic::PanicInfo,
-    sync::atomic::{AtomicUsize, Ordering},
-};
-
-static PANIC_COUNT: AtomicUsize = AtomicUsize::new(0);
+use core::panic::PanicInfo;
 
 #[panic_handler]
 pub(crate) fn panic(info: &PanicInfo) -> ! {
     let mut stderr = io::stderr();
 
-    // prevent stack overflow from panics within panics.
-    if PANIC_COUNT.fetch_add(1, Ordering::SeqCst) > 0 {
-        // bypass anything that could panic.
-        let _ = writeln!(
-            &mut stderr.inner.inner,
-            "\x1b[38;5;9mmocha:\x1b[m attempted panic within panic"
-        );
-    } else {
-        let _ = writeln!(&mut stderr, "\x1b[38;5;9mmocha:\x1b[m panic: {info}");
-    }
-
+    let _ = writeln!(&mut stderr, "\x1b[38;5;9mmocha:\x1b[m panic: {info}");
     let _ = stderr.flush();
 
     process::exit(1)
